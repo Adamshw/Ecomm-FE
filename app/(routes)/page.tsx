f13dce3318a3822533f8d993d412d70b8cae6911
adamshw <adam.shwehne@gmail.com>
2023-09-25 19:16:08 +0200

    added cart and billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,27 +1,27 @@
-// import getBillboard from "@/actions/get-billboard";
+import getBillboard from "@/actions/get-billboard";
 // import getProducts from "@/actions/get-products";
 // import ProductList from "@/components/product-list";
-// import Billboard from "@/components/ui/billboard";
-// import Container from "@/components/ui/container";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
     //   const products = await getProducts({ isFeatured: true });
-    //   const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+      const billboard = await getBillboard("e10a13c8-7d56-4b79-ae7a-c842a0096d29");
 
     return (
-        <div>hello store</div>
-        // <Container>
-        //   <div className="space-y-10 pb-10">
-        //     <Billboard 
-        //       data={billboard}
-        //     />
-        //     <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        //       <ProductList title="Featured Products" items={products} />
-        //     </div>
-        //   </div>
-        // </Container>
+
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard
+                    data={billboard}
+                />
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    {/* <ProductList title="Featured Products" items={products} /> */}
+                </div>
+            </div>
+        </Container>
     )
 };
 
